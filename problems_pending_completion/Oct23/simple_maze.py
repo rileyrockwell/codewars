1ba1a336011dfa03c779005d79713e7b022956cf
Riley Rockwell <64986809+rileyrockwell@users.noreply.github.com>
2023-10-23 12:18:05 -0700

    updates

---
+++ problems_pending_completion/Oct23/simple_maze.py
@@ -0,0 +1,6 @@
+def has_exit(maze):
+	return 1
+
+
+maze = ["# ##", "# k#", "####"]
+print(has_exit(maze))
---
+++ problems_pending_completion/route_calculator.py
@@ -0,0 +1,5 @@
+def calculate(expression):
+	return expression
+
+expression = "f(x) = x**3"
+print(calculate(expression))
